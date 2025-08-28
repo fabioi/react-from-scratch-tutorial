3512ab6fd2168f46e8f9ba9c01d9f7a56b2f8c90
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-08-28 12:58:52 +0200

    Update `App` and `index.html` for improved styling and metadata
    - Removed `font-mono` class from `<h1>` in `App.jsx`.
    - Added viewport meta tag, updated title, and included Google Fonts in `index.html` for better design consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,6 @@ const tech = "React tailwind and Vite";
 // dato che `App` viene usata nello stesso file in cui è definita per essere renderizzata, non è necessario esportarla
 export function App() {
     return (
-        <h1 className="font-mono text-4xl">Hello {tech}!</h1>
+        <h1 className="text-4xl">Hello {tech}!</h1>
     )
 }
