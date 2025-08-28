086f4681ff8e5ac7c481cc071769d5f8becd31a7
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-08-28 13:23:09 +0200

    Refactor `App` by introducing `Container` component and improving `Header` structure for better readability and reusability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
 // Definizione del componente principale App
 // dato che `App` viene usata nello stesso file in cui è definita per essere renderizzata, non è necessario esportarla
 export function App() {
-    return (
-        <main className="min-h-dvh bg-gradient-to-b from-cyan-200 to-white to-[60vh]">
-            {/* Max-width container */}
-            <div className="mx-auto max-w-5xl p-4 md:p-8">
-                {/* Header */}
-                <Header/>
-            </div>
-        </main>
-    );
+    return (<main className="min-h-dvh bg-gradient-to-b from-cyan-200 to-white to-[60vh]">
+        {/* Max-width container */}
+        <Container>
+            <Header />
+        </Container>
+    </main>);
 }
 
 
-function Header(){
+function Header() {
+    return (<>
+            {/* Header */}
+            <header>
+                {/* Logo */}
+                <a className="group" href="/public">
+                    <div className="inline-flex items-center gap-4">
+                        <img
+                            src="/images/logo.png"
+                            alt="DevPups"
+                            className="h-16 transition group-hover:scale-105 group-hover:-rotate-6 md:h-20 lg:h-24"
+                        />
+                        <p className="text-lg font-semibold">Dev Pups</p>
+                    </div>
+                </a>
+                {/* Hero copy */}
+                <div className="mt-6">
+                    <h1 className="text-lg font-bold">We've got the best puppies!</h1>
+                    <p className="text-slate-600">
+                        Don't take our word — let the pictures do the talking :)
+                    </p>
+                </div>
+            </header>
+        </>
+    )
+}
+
+
+function Container({children}) {
     return (
-        <>
-        {/* Header */}
-    <Header>
-        {/* Logo */}
-        <a className="group" href="/public">
-            <div className="inline-flex items-center gap-4">
-                <img
-                    src="/images/logo.png"
-                    alt="DevPups"
-                    className="h-16 transition group-hover:scale-105 group-hover:-rotate-6 md:h-20 lg:h-24"
-                />
-                <p className="text-lg font-semibold">Dev Pups</p>
-            </div>
-        </a>
-        {/* Hero copy */}
-        <div className="mt-6">
-            <h1 className="text-lg font-bold">We've got the best puppies!</h1>
-            <p className="text-slate-600">
-                Don't take our word — let the pictures do the talking :)
-            </p>
+        <div className="mx-auto max-w-5xl p-4 md:p-8">
+            {children}
         </div>
-    </Header>
-            </>
     )
 }
