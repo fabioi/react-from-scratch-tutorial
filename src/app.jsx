0ee5b38c4d8db481441a216f6b15399d2a5e6f7c
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-08-28 11:55:31 +0200

    Wrap `App` in `StrictMode` for better development checks and update `React` import in `app.jsx`.

--- src/app.jsx
+++ src/app.jsx
@@ -12,7 +12,7 @@
 // Import fondamentali per ogni applicazione React:
 // - React: libreria core per creare componenti e gestire la UI
 // - ReactDOM: libreria per manipolare il DOM e renderizzare React nel browser
-import React from 'react'
+import React, {StrictMode} from 'react'
 import ReactDOM, {createRoot} from 'react-dom/client'
 import './app.css'
 
@@ -30,8 +30,11 @@ function App() {
 // ReactDOM.createRoot() crea un contenitore React che gestisce l'aggiornamento efficiente
 // del DOM tramite il Virtual DOM. È il metodo predefinito in React 18+ per il rendering.
 
-createRoot(document.getElementById('root')).render(<App/>)
-
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+);
 
 
 // Renderizza il componente App all'interno della root
