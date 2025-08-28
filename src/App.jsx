dec533c96b7ce8ae14f5bc82fed2758ee1166249
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-08-28 13:11:45 +0200

    Refactor `App` component by extracting `Header` into a separate function for improved readability and reuse

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,37 @@ export function App() {
             {/* Max-width container */}
             <div className="mx-auto max-w-5xl p-4 md:p-8">
                 {/* Header */}
-                <header>
-                    {/* Logo */}
-                    <a className="group" href="/public">
-                        <div className="inline-flex items-center gap-4">
-                            <img
-                                src="/images/logo.png"
-                                alt="DevPups"
-                                className="h-16 transition group-hover:scale-105 group-hover:-rotate-6 md:h-20 lg:h-24"
-                            />
-                            <p className="text-lg font-semibold">Dev Pups</p>
-                        </div>
-                    </a>
-                    {/* Hero copy */}
-                    <div className="mt-6">
-                        <h1 className="text-lg font-bold">We've got the best puppies!</h1>
-                        <p className="text-slate-600">
-                            Don't take our word — let the pictures do the talking :)
-                        </p>
-                    </div>
-                </header>
+                <Header/>
             </div>
         </main>
     );
+}
+
+
+function Header(){
+    return (
+        <>
+        {/* Header */}
+    <Header>
+        {/* Logo */}
+        <a className="group" href="/public">
+            <div className="inline-flex items-center gap-4">
+                <img
+                    src="/images/logo.png"
+                    alt="DevPups"
+                    className="h-16 transition group-hover:scale-105 group-hover:-rotate-6 md:h-20 lg:h-24"
+                />
+                <p className="text-lg font-semibold">Dev Pups</p>
+            </div>
+        </a>
+        {/* Hero copy */}
+        <div className="mt-6">
+            <h1 className="text-lg font-bold">We've got the best puppies!</h1>
+            <p className="text-slate-600">
+                Don't take our word — let the pictures do the talking :)
+            </p>
+        </div>
+    </Header>
+            </>
+    )
 }
