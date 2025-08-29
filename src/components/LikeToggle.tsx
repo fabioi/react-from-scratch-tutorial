c5637a9c54d2b6303dfb56850b522055472ccbfb
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-08-29 13:36:03 +0200

    Add `useState` hook to manage `isLiked` state in `LikeToggle` and implement toggle functionality for the `Heart` icon.

--- src/components/LikeToggle.tsx
+++ src/components/LikeToggle.tsx
@@ -1,14 +1,28 @@
 import {Heart} from "lucide-react";
 import {type Puppy} from "../types";
+import * as React from "react";
+
 
 export function LikeToggle({puppy}: { puppy: Puppy }) {
+    // We have destructured the react use state hook here
+    // This is the react hook because allows to hook in the way  react internally re-renders components
+    const [isLiked, setIsLiked] = React.useState(false);
+
+    // In JS is possible to to use private functions inside other functions
+    function handleClick(event) {
+        setIsLiked(!isLiked); // Cambia lo stato da true a false o viceversa
+    }
+
     return (
-        <button className="group">
+        <button className="group" onClick={handleClick}>
             <Heart className={
-                true ?
+                isLiked ?
                     "fill-pink-500 stroke-none" :
                     "stroke-slate-200 group-hover:stroke-slate-300"
             }/>
         </button>
     );
 }
+
+
+
