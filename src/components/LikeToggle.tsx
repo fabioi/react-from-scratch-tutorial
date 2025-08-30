24685834385080c1902696dd1871784ecc58b5d0
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-08-30 15:50:56 +0200

    Refactor `LikeToggle` to simplify `useState` import and inline `handleClick` logic.

--- src/components/LikeToggle.tsx
+++ src/components/LikeToggle.tsx
@@ -1,20 +1,18 @@
 import {Heart} from "lucide-react";
 import {type Puppy} from "../types";
-import * as React from "react";
+import {useState} from "react";
 
 
 export function LikeToggle({puppy}: { puppy: Puppy }) {
     // We have destructured the react use state hook here
     // This is the react hook because allows to hook in the way  react internally re-renders components
-    const [isLiked, setIsLiked] = React.useState(false);
-
-    // In JS is possible to to use private functions inside other functions
-    function handleClick(event) {
-        setIsLiked(!isLiked); // Cambia lo stato da true a false o viceversa
-    }
+    const [isLiked, setIsLiked] = useState(false);
 
     return (
-        <button className="group" onClick={handleClick}>
+        <button className="group" onClick={() =>
+            setIsLiked(!isLiked)
+            // can add more functions here
+        }>
             <Heart className={
                 isLiked ?
                     "fill-pink-500 stroke-none" :
