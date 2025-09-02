48f2d3c98f8d6113538d10535764dd492f96d5f8
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-09-02 15:56:58 +0200

    Replace inline delete SVG in `Search` component with `LucideDelete` from `lucide-react`

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,5 @@
 import {Dispatch, SetStateAction} from "react";
+import {LucideDelete} from "lucide-react";
 
 export function Search({searchQuery, setSearchQuery}:{
     searchQuery: string;
@@ -22,23 +23,7 @@ export function Search({searchQuery, setSearchQuery}:{
                         />
                         <button
                             className="inline-block rounded bg-cyan-300 px-4 py-2 !pr-3 !pl-2.5 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width={24}
-                                height={24}
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth={2}
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                className="lucide lucide-delete"
-                            >
-                                <path
-                                    d="M10 5a2 2 0 0 0-1.344.519l-6.328 5.74a1 1 0 0 0 0 1.481l6.328 5.741A2 2 0 0 0 10 19h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2z"/>
-                                <path d="m12 9 6 6"/>
-                                <path d="m18 9-6 6"/>
-                            </svg>
+                            <LucideDelete onClick={() => setSearchQuery("")}/>
                         </button>
                     </div>
                 </div>
