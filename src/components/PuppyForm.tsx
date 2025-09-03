92b32e735ce0bb91c9dbf9e3f052f55ac02c96db
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-09-03 12:05:20 +0200

    Enhance `SubmitButton` in `PuppyForm` to display a loading spinner during submission and disable the button when pending. This improves user feedback during async operations.

--- src/components/PuppyForm.tsx
+++ src/components/PuppyForm.tsx
@@ -1,3 +1,4 @@
+import { LoaderCircle } from "lucide-react";
 import {Puppy} from "../types";
 import {Dispatch, SetStateAction} from "react";
 import {useFormStatus} from "react-dom";
@@ -74,10 +75,11 @@ function SubmitButton() {
 
     return (
         <button
-            className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
+            className="mt-4 inline-block rounded bg-cyan-300 px-4 py-2 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none disabled:cursor-not-allowed"
             type="submit"
+            disabled={status.pending}
         >
-            {status.pending ? 'Adding puppet...' : 'Added puppet!'}
+            {status.pending ? <LoaderCircle className="animate-spin stroke-slate-300"/> : 'Added puppet!'}
 
         </button>
     );
